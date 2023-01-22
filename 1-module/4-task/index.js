d51a9e8e33ba1c31390888d7b39b8b1c17620fa5
AleksanderNisenbaum <nisenblvm1@gmail.com>
2023-01-22 12:43:09 +0200

    решение про Uppercase

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,11 @@
 function checkSpam(str) {
   // ваш код...
+  str = str.toUpperCase();
+  let str1 = "1xBet";
+  let str2 = "XXX";
+  if(str.includes(str1.toUpperCase()) || str.includes(str2.toUpperCase())){
+    return(true);
+  }else{
+    return(false);
+  }
 }
