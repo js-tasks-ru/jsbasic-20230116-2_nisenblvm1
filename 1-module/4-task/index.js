d519ad090932eeaa80922054d8954677ceac3980
AleksanderNisenbaum <nisenblvm1@gmail.com>
2023-01-23 19:08:43 +0200

    Избавился от конструкции if

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -3,9 +3,10 @@ function checkSpam(str) {
   str = str.toUpperCase();
   let str1 = "1xBet";
   let str2 = "XXX";
-  if(str.includes(str1.toUpperCase()) || str.includes(str2.toUpperCase())){
-    return(true);
-  }else{
-    return(false);
-  }
+  // if(str.includes(str1.toUpperCase()) || str.includes(str2.toUpperCase())){
+  //   return(true);
+  // }else{
+  //   return(false);
+  // }
+  return(str.includes(str1.toUpperCase()) || str.includes(str2.toUpperCase()))
 }
