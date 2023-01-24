70f0ae0efd8ac66c10e7086d601e2a656e711484
AleksanderNisenbaum <nisenblvm1@gmail.com>
2023-01-24 15:40:46 +0200

    obj calculator

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,15 @@
 let calculator = {
   // ваш код
+  read(x, y){
+    this.x = x;
+    this.y = y;
+  },
+  sum(){
+    return(this.x + this.y);
+  },
+  mul(){
+    return(this.x * this.y);
+  },
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
