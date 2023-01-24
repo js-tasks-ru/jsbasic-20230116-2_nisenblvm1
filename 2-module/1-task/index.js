06e286bc52c65e0f225f3c8956092632eab63472
AleksanderNisenbaum <nisenblvm1@gmail.com>
2023-01-24 14:37:00 +0200

    Про подсчет общей суммы зп

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,10 @@
 function sumSalary(salaries) {
   // ваш код...
+  let count = 0;
+  for (let key in salaries){
+    if(isFinite(salaries[key]) && (typeof(salaries[key]) !== 'Boolean')){
+      count += salaries[key];
+    }
+  }
+  return(count)
 }
