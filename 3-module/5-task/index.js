f8fbb225d38a9e14809f8ebf0f860ca7a9f37dea
AleksanderNisenbaum <nisenblvm1@gmail.com>
2023-01-29 16:15:58 +0200

    getMinMax function

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,8 @@
 function getMinMax(str) {
   // ваш код...
+  let arrNumbers = ((str.split(' ')).filter(item => !isNaN(item))).map(item => Number(item));
+  let result = {};
+  result.min = Math.min(...arrNumbers);
+  result.max = Math.max(...arrNumbers);
+  return(result)
 }
