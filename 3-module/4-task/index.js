ef0d2b88a380696042b47895258ef831fafd1de6
AleksanderNisenbaum <nisenblvm1@gmail.com>
2023-01-29 13:12:43 +0200

    salary names

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,9 @@
 function showSalary(users, age) {
   // ваш код...
+  let arrRes = [];
+  users.forEach((item) => {
+    if(item.age <= age){arrRes.push(item.name + ',' + " " + item.balance + '\n')}
+  })
+  let newStr = (arrRes.join(''));
+  return(newStr.slice(0, -1));
 }
