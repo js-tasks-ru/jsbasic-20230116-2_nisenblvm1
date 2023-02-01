b6361a892d108585fe119b012b4cce88efdb8d4e
AleksanderNisenbaum <nisenblvm1@gmail.com>
2023-02-01 11:43:56 +0200

    fixed 3-modul 4-task

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,9 +1,4 @@
 function showSalary(users, age) {
-  // ваш код...
-  let arrRes = [];
-  users.forEach((item) => {
-    if(item.age <= age){arrRes.push(item.name + ',' + " " + item.balance + '\n')}
-  })
-  let newStr = (arrRes.join(''));
-  return(newStr.slice(0, -1));
+ 
+  return(users.filter(obj => (obj.age <= age)).map(obj => (obj.name + ', ' + obj.balance)).join('\n'));
 }
