554325130680022cf34522506dad1f0921c648ba
AleksanderNisenbaum <nisenblvm1@gmail.com>
2023-02-02 13:15:45 +0200

    4-modul 1-task

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,7 +1,7 @@
 function getMinMax(str) {
   // ваш код...
   let arrNumbers = ((str.split(' ')).filter(item => !isNaN(item))).map(item => Number(item));
-  
+
   return { min: Math.min(...arrNumbers),
     max: Math.max(...arrNumbers)};
 }
